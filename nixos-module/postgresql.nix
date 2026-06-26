f9f84647cb266059df075cf4c57ced24d9825275
Plume <leana.jiang+git@icloud.com>
2026-06-26 10:24:33 +0200

    create nixos module (#1)
    * create nixos module
    
    * fix service attribute

---
+++ nixos-module/postgresql.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+{
+  assertions = [
+    {
+      assertion = pkgs ? postgresql_17;
+      message = ''
+        Flora needs postgresql version 17 to work.
+      '';
+    }
+  ];
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_17;
+    authentication = lib.mkOverride 10 ''
+      local all       all     trust
+      host  all       all     127.0.0.1/32   scram-sha-256
+      host  all       all     ::1/128        scram-sha-256
+    '';
+  };
+}
